397aa99332cfc2acf49cd0a0d6045361400039e3
hernane <hernaneprates@hotmail.com>
2020-11-23 23:05:01 +0100

    initial commit

---
+++ src/main/java/com/renotest/renotest/RenotestApplication.java
@@ -0,0 +1,13 @@
+package com.renotest.renotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renotest/renotest/RenotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renotest.renotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
